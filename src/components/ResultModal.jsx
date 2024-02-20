2600035011e1eeec0ae360ef762effff9fb5e8d6
alkuma <alok.kumar@transactiveltd.com>
2024-02-20 13:04:38 +0530

    portal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {createPortal} from 'react-dom';
 const ResultModal =  forwardRef( function ResultModal({ targetTime, remainingTime, onReset}, ref) {
     const dialog = useRef();
 
@@ -16,7 +17,7 @@ const ResultModal =  forwardRef( function ResultModal({ targetTime, remainingTim
          }
         };
     });
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             { userLost && <h2>You Lost </h2>}
             { !userLost && <h2>You Score: {score} </h2>}
@@ -29,7 +30,8 @@ const ResultModal =  forwardRef( function ResultModal({ targetTime, remainingTim
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 })
 
